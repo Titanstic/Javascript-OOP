673ee3355d1765ca0086b25caf5dbdd80f022401
Titasntic <mgminkhant301999@gmail.com>
2022-06-28 12:14:57 +0630

    2 files modified

--- helper.js
+++ helper.js
@@ -2,10 +2,10 @@ export function person() {
   console.log('show your name');
 }
 
-export class Car {
-  static PI = 3.14;
-
-  static add(a, b) {
-    return a + b;
+export let Car = class {
+  color = 'red';
+  wheel = 4;
+  drive() {
+    console.log(`car color is ${this.color}`); 
   }
-}
+};
--- index.js
+++ index.js
@@ -1,6 +1,83 @@
-import { person, Car } from './helper.js';
+// import { person, Car } from './helper.js';
 
-person();
+// // person();
 
-console.log(Car.PI);
-console.log(Car.add(2, 5));
+// let car = new Car();
+
+// // console.log(car.#hp);
+// console.log(car.color);
+// car.drive();
+
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   run() {
+//     console.log(`${this.name} is running`);
+//   }
+// }
+
+// class Dog extends Animal {
+//   bark() {
+//     console.log(`${this.name} is woof ... woof ..`);
+//   }
+// }
+
+// class Cat extends Animal {
+//   constructor(name, color) {
+//     super(name);
+//     this.color = color;
+//   }
+
+//   meow() {
+//     console.log(`${this.name} is cat`);
+//   }
+// }
+
+// let animal = new Dog('mg mg');
+// animal.run();
+// animal.bark();
+
+// let cat = new Cat('shwe war', 'yellow');
+// console.log(cat.color);
+// cat.meow();
+
+// function add1000() {
+//   let result = 0;
+//   for (let i = 0; i < 1000; i++) {
+//     result += i;
+//   }
+//   return result;
+// }
+
+// function add1000later() {
+//   return new Promise((resolve, reject) => {
+//     let result = add1000();
+//     if (result) resolve(result);
+//     else reject();
+//   });
+// }
+
+// add1000later()
+//   .then((result) => result + 1000)
+//   .then((result) => console.log(result))
+//   .catch(console.log('error'));
+
+fetch('https://api.covid19api.com/summary')
+  .then((res) => res.json())
+  .then((data) => {
+    const global = data.Global;
+    const allCountries = data.Countries;
+    const myanmar = allCountries.find((c) => c.Country === 'Myanmar');
+    console.log(myanmar);
+  });
+
+async function covidInfo() {
+  const response = await fetch('https://api.covid19api.com/summary');
+  const data = await response.json();
+  const global = data.Global;
+  const allCountries = data.Countries;
+  const myanmar = allCountries.find((c) => c.Country === 'Myanmar');
+  console.log(myanmar);
+}
